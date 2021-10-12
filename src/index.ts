0f3414ff254a83da92618ec30983d111475034c9
Jeff Wu <jeffywu@pm.me>
2021-10-12 05:36:53 -0700

    exporting types

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,25 @@ import Notional from './Notional';
 import TypedBigNumber, {BigNumberType} from './libs/TypedBigNumber';
 import {SystemEvents} from './system/System';
 import {AccountEvents} from './account/AccountRefresh';
+import {Notional as NotionalProxy} from './typechain/Notional';
+import {NoteERC20} from './typechain/NoteERC20';
+import {NTokenERC20} from './typechain/NTokenERC20';
+import {Governor} from './typechain/Governor';
+import {IAggregator} from './typechain/IAggregator';
+import {ERC20} from './typechain/ERC20';
+import {AssetRateAggregator} from './typechain/AssetRateAggregator';
 
 export default Notional;
 export {
-  TypedBigNumber, BigNumberType, SystemEvents, AccountEvents,
+  TypedBigNumber,
+  BigNumberType,
+  SystemEvents,
+  AccountEvents,
+  NotionalProxy,
+  NoteERC20,
+  Governor,
+  NTokenERC20,
+  IAggregator,
+  ERC20,
+  AssetRateAggregator,
 };
