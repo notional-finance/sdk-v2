0bafe7298eba47caa2791cb79cb4e5c9e8be7c8a
Jeff Wu <jeffywu@pm.me>
2021-10-12 08:14:03 -0700

    Squashed commit of the following:
    commit 92975c8a20aa947b90cc7ffff676c4b493dfdc47
    Author: Jeff Wu <jeffywu@pm.me>
    Date:   Tue Oct 12 08:13:36 2021 -0700
    
        fixing packaging issues

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,6 @@ import Notional from './Notional';
 import TypedBigNumber, {BigNumberType} from './libs/TypedBigNumber';
 import {SystemEvents} from './system/System';
 import {AccountEvents} from './account/AccountRefresh';
-import {Notional as NotionalProxy} from './typechain/Notional';
-import {NoteERC20} from './typechain/NoteERC20';
-import {NTokenERC20} from './typechain/NTokenERC20';
-import {Governor} from './typechain/Governor';
-import {IAggregator} from './typechain/IAggregator';
-import {ERC20} from './typechain/ERC20';
-import {AssetRateAggregator} from './typechain/AssetRateAggregator';
 
 export default Notional;
 export {
@@ -16,11 +9,4 @@ export {
   BigNumberType,
   SystemEvents,
   AccountEvents,
-  NotionalProxy,
-  NoteERC20,
-  Governor,
-  NTokenERC20,
-  IAggregator,
-  ERC20,
-  AssetRateAggregator,
 };
