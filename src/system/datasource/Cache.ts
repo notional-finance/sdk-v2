1a40c8a3784ae79425c01fbc6c26e9d198f703f5
Jeff Wu <jeffywu@pm.me>
2021-11-12 15:36:38 -0800

    first version of cache

--- src/system/datasource/Cache.ts
+++ src/system/datasource/Cache.ts
@@ -1,5 +1,7 @@
+import { BigNumber } from 'ethers';
 import EventEmitter from 'eventemitter3';
 import {DataSource} from '.';
+import TypedBigNumber from '../../libs/TypedBigNumber';
 
 export default class Cache extends DataSource {
   constructor(
@@ -9,7 +11,58 @@ export default class Cache extends DataSource {
     super(eventEmitter, refreshIntervalMS);
   }
 
+  private parseMap(_: any, value: any) {
+    if (typeof value === 'object' && value !== null) {
+      if (value.dataType === 'Map') {
+        return new Map(value.value);
+      }
+    }
+
+    return value;
+  }
+
+  async getCacheData(): Promise<any> {
+    return {}
+  }
+
   async refreshData() {
-    console.log('refresh');
+    const parsedObject = JSON.parse(await this.getCacheData(), this.parseMap)
+    parsedObject.ethRateData.forEach((value: any, key: number) => {
+      this.ethRateData.set(key, BigNumber.from(value))
+    })
+    parsedObject.assetRateData.forEach((value: any, key: number) => {
+      this.assetRateData.set(key, BigNumber.from(value))
+    })
+    parsedObject.nTokenAssetCashPV.forEach((value: any, key: number) => {
+      this.nTokenAssetCashPV.set(key, TypedBigNumber.fromObject(value))
+    })
+    parsedObject.nTokenTotalSupply.forEach((value: any, key: number) => {
+      this.nTokenTotalSupply.set(key, TypedBigNumber.fromObject(value))
+    })
+    parsedObject.nTokenIncentiveFactors.forEach((value: any, key: number) => {
+      this.nTokenIncentiveFactors.set(key, {
+        integralTotalSupply: BigNumber.from(value.integralTotalSupply),
+        lastSupplyChangeTime: BigNumber.from(value.lastSupplyChangeTime)
+      })
+    })
+    parsedObject.nTokenCashBalance.forEach((value: any, key: number) => {
+      this.nTokenCashBalance.set(key, TypedBigNumber.fromObject(value))
+    })
+    parsedObject.nTokenLiquidityTokens.forEach((value: any, key: number) => {
+      const newValues = value.map((v: any) => {
+        v.notional = TypedBigNumber.fromObject(v.notional)
+        return v
+      })
+      this.nTokenLiquidityTokens.set(key, newValues)
+    })
+    parsedObject.nTokenfCash.forEach((value: any, key: number) => {
+      const newValues = value.map((v: any) => {
+        v.notional = TypedBigNumber.fromObject(v.notional)
+        return v
+      })
+      this.nTokenfCash.set(key, newValues)
+    })
+    this.lastUpdateBlockNumber = parsedObject.lastUpdateBlockNumber
+    this.lastUpdateTimestamp = parsedObject.lastUpdateTimestamp
   }
 }
---
+++ tests/unit/CacheRefresh.test.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import EventEmitter from 'eventemitter3';
+import Cache from '../../src/system/datasource/Cache';
+import { System } from '../../src/system';
+
+describe('Cache Refresh', () => {
+  const eventEmitter = new EventEmitter()
+  const cache: Cache = new Cache(eventEmitter, 10000)
+  const mockResult = fs.readFileSync(__dirname + '/../mocks/MockCacheResult.json').toString()
+  System.overrideSystem(({
+    getCurrencyBySymbol: () => {
+      return {id: 1}
+    }
+  } as unknown) as System);
+
+   beforeEach(() => {
+      jest.spyOn(cache, 'getCacheData').mockResolvedValue(mockResult);
+    });
+
+  it('should return cached data', (done) => {
+    cache.refreshData().then(() => {
+      console.log(cache.nTokenfCash)
+      done()
+    })
+  })
+
+})
