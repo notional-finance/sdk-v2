bb556c2678e522d8a692bf7e875af46cf1827bc8
Hugo Boisselle-Morin <hugo.boisselle@gmail.com>
2021-12-17 15:07:37 -0500

    Add FreeCollateral to exports

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import TypedBigNumber, {BigNumberType} from './libs/TypedBigNumber';
 import {SystemEvents} from './system/System';
 import {AccountEvents} from './account/AccountRefresh';
 import {Account, AccountData} from './account';
+import FreeCollateral from './system/FreeCollateral';
 
 export default Notional;
 export * from './libs/types';
 export {
+  FreeCollateral,
   TypedBigNumber,
   BigNumberType,
   SystemEvents,
