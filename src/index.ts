51a40a345a34395dfbdd16e3df6e39d73b87c7f9
Hugo Boisselle-Morin <hugo.boisselle@gmail.com>
2021-12-17 16:37:22 -0500

    export NTokenValue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Notional from './Notional';
 import TypedBigNumber, {BigNumberType} from './libs/TypedBigNumber';
 import {SystemEvents} from './system/System';
+import {NTokenValue} from './system';
 import {AccountEvents} from './account/AccountRefresh';
 import {Account, AccountData} from './account';
 import FreeCollateral from './system/FreeCollateral';
@@ -9,6 +10,7 @@ export default Notional;
 export * from './libs/types';
 export {
   FreeCollateral,
+  NTokenValue,
   TypedBigNumber,
   BigNumberType,
   SystemEvents,
