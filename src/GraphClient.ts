22511109f890e29e0fcf486e8045ea4c7b7fe27c
Jeff Wu <jeffywu@pm.me>
2021-12-22 07:09:44 -0800

    adding a helper for graph batch queries

--- src/GraphClient.ts
+++ src/GraphClient.ts
@@ -8,6 +8,7 @@ import {
   DocumentNode,
   OperationVariables,
   TypedDocumentNode,
+  gql,
 } from '@apollo/client/core';
 import {onError} from '@apollo/client/link/error';
 import {RetryLink} from '@apollo/client/link/retry';
@@ -45,5 +46,77 @@ export default class GraphClient {
     }
 
     return result.data;
+  }
+
+  /**
+   * Uses the recommended more performant batch query method here:
+   * https://thegraph.com/docs/en/developer/graphql-api#pagination
+   *
+   * The $id should be used as filter so that it hits the underlying index and we get
+   * a faster query. Need to save the largest ID per query. Queries need to have the variable
+   * $lastID in order to work with this method:
+   *
+   * {
+   *    query allAccounts($lastID: String) {
+   *      batch: accounts(
+   *          first: $pageSize,
+   *          where: { id_gt: $lastID },
+   *          orderBy: id, orderDirection: asc
+   *      ) {
+   *        id
+   *        owner
+   *      }
+   *    }
+   *  }
+   *
+   * @param query
+   * @param variables
+   * @returns
+   */
+  public async batchQuery<TData = any, TVariables = OperationVariables>(
+    query: DocumentNode | TypedDocumentNode<TData, TVariables>,
+    variables?: TVariables,
+    initialID: string = '0',
+    listKey: string = 'batch',
+    pageSize = 1000,
+  ) {
+    let hasMoreResults = false;
+    let lastID = initialID;
+    const batchResult: any[] = [];
+
+    do {
+      // eslint-disable-next-line no-await-in-loop
+      const results = await this.queryOrThrow<TData, TVariables>(
+        query,
+        {...variables, pageSize, lastID} as unknown as TVariables,
+      );
+      batchResult.push(...results[listKey]);
+
+      hasMoreResults = results[listKey].length > 0;
+      if (hasMoreResults) lastID = results[listKey][results[listKey].length - 1].id;
+    } while (hasMoreResults);
+
+    return batchResult;
+  }
+
+  /**
+   * Builds a query compatible with `batchQuery`
+   * @param objectName
+   * @param queryBody
+   * @returns DocumentNode
+   */
+  public makeBatchQuery(
+    objectName: string,
+    queryBody: string,
+  ) {
+    return gql`
+      query batchQuery($pageSize: Int!, $lastID: String) {
+        batch: ${objectName}(
+          first: $pageSize,
+          where: { id_gt: $lastID },
+          orderBy: id, orderDirection: asc
+        ) ${queryBody}
+      }
+    `;
   }
 }
