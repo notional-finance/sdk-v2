dbd5b54e1a313d732aa61deeaad765287bcce53c
Jeff Wu <jeffywu@pm.me>
2022-07-09 07:03:39 -0700

    Feature/batch lend (#93)
    * adding batch lend and unit tests
    
    * adding batch lend test

--- src/Notional.ts
+++ src/Notional.ts
@@ -74,7 +74,7 @@ export default class Notional extends TransactionBuilder {
     public provider: ethers.providers.Provider,
     public contracts: Contracts,
   ) {
-    super(system.getNotionalProxy(), system);
+    super();
   }
 
   public static getContracts(addresses: Addresses, signer: Signer): Contracts {
