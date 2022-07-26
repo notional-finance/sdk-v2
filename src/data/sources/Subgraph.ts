eb34a2d04c3ef95c0682f5251c698ab6a55cb999
Jeff Wu <jeffywu@pm.me>
2022-07-26 06:20:40 -0700

    getting vault data from subgraph

--- src/data/sources/Subgraph.ts
+++ src/data/sources/Subgraph.ts
@@ -4,6 +4,7 @@ import { TokenType } from '../../libs/types';
 import GraphClient from '../GraphClient';
 import { SerializedContract } from '../encoding/SystemProto';
 import TypedBigNumber, { BigNumberType } from '../../libs/TypedBigNumber';
+import { SecondaryBorrowArray } from '..';
 
 const systemConfigurationQuery = gql`
   {
@@ -71,6 +72,8 @@ const systemConfigurationQuery = gql`
         maxBorrowMarketIndex
         secondaryBorrowCurrencies {
           id
+          symbol
+          underlyingSymbol
         }
         enabled
         allowRollPosition
@@ -101,7 +104,6 @@ const systemConfigurationQuery = gql`
           settlementTimestamp
           settlementStrategyTokenValue
           settlementSecondaryBorrowfCashSnapshot
-          settlementSecondaryBorrowExchangeRate
           settlementRate {
             rate
           }
@@ -179,6 +181,8 @@ interface SystemQueryResult {
       maxBorrowMarketIndex: number;
       secondaryBorrowCurrencies: {
         id: string;
+        symbol: string;
+        underlyingSymbol: string | null;
       }[];
       enabled: boolean;
       allowRollPosition: boolean;
@@ -210,6 +214,9 @@ interface SystemQueryResult {
         settlementSecondaryBorrowfCashSnapshot: [string, string] | null;
         settlementSecondaryBorrowExchangeRate: [string, string] | null;
         shortfall: string | null;
+        settlementRate: {
+          rate: string;
+        } | null;
         remainingSettledAssetCash: string | null;
         remainingSettledStrategyTokens: string | null;
       }[];
@@ -289,21 +296,21 @@ export interface CurrencyConfig {
     allowsReentrancy: boolean;
     maxPrimaryBorrowCapacity: TypedBigNumber;
     totalUsedPrimaryBorrowCapacity: TypedBigNumber;
-    // maxSecondaryBorrowCapacity: [TypedBigNumber, TypedBigNumber] | null;
-    // totalUsedSecondaryBorrowCapacity: [TypedBigNumber, TypedBigNumber] | null;
-    maturities: {
+    maxSecondaryBorrowCapacity: SecondaryBorrowArray;
+    totalUsedSecondaryBorrowCapacity: SecondaryBorrowArray;
+    vaultStates: {
       maturity: number;
       totalPrimaryfCashBorrowed: TypedBigNumber;
       totalAssetCash: TypedBigNumber;
       totalVaultShares: TypedBigNumber;
       totalStrategyTokens: TypedBigNumber;
-      // totalSecondaryfCashBorrowed: [TypedBigNumber, TypedBigNumber] | null;
-      // totalSecondaryDebtShares: [TypedBigNumber, TypedBigNumber] | null;
+      totalSecondaryfCashBorrowed: SecondaryBorrowArray;
+      totalSecondaryDebtShares: SecondaryBorrowArray;
       isSettled: boolean;
       settlementTimestamp: number | undefined;
       settlementStrategyTokenValue: TypedBigNumber | undefined;
-      // settlementSecondaryBorrowfCashSnapshot: [TypedBigNumber, TypedBigNumber] | null;
-      // settlementSecondaryBorrowExchangeRate: [TypedBigNumber, TypedBigNumber] | null;
+      settlementSecondaryBorrowfCashSnapshot: SecondaryBorrowArray;
+      settlementRate: BigNumber | undefined;
       shortfall: TypedBigNumber | undefined;
       remainingSettledAssetCash: TypedBigNumber | undefined;
       remainingSettledStrategyTokens: TypedBigNumber | undefined;
@@ -311,10 +318,56 @@ export interface CurrencyConfig {
   }[];
 }
 
-function mapSecondaryBorrowCurrencies(s?: { id: string }[]) {
+function mapSecondaryBorrowCurrencies(s?: { id: string; symbol: string; underlyingSymbol: string | null }[]) {
   if (!s || s.length === 0) return undefined;
   if (s.length === 1) return [Number(s[0].id), 0];
   if (s.length === 2) return [Number(s[0].id), Number(s[1].id)];
+  return undefined;
+}
+
+function mapSecondarySymbols(
+  s?: { id: string; symbol: string; underlyingSymbol: string | null }[]
+): [string | undefined, string | undefined] | undefined {
+  if (!s || s.length === 0) return undefined;
+  if (s.length === 1) return [s[0].underlyingSymbol || s[0].symbol, undefined];
+  if (s.length === 2) return [s[0].underlyingSymbol || s[0].symbol, s[1].underlyingSymbol || s[1].symbol];
+  return undefined;
+}
+
+function mapSecondaryTypedBigNumber(
+  v: [string, string] | null,
+  c: [string | undefined, string | undefined] | undefined
+) {
+  if (!v) return undefined;
+  if (!c) return undefined;
+  if (c[0] !== undefined && c[1] === undefined) {
+    return [TypedBigNumber.from(v[0], BigNumberType.InternalUnderlying, c[0]).toJSON()];
+  }
+  if (c[0] !== undefined && c[1] !== undefined) {
+    return [
+      TypedBigNumber.from(v[0], BigNumberType.InternalUnderlying, c[0]).toJSON(),
+      TypedBigNumber.from(v[1], BigNumberType.InternalUnderlying, c[1]).toJSON(),
+    ];
+  }
+  return undefined;
+}
+
+function mapSecondaryDebtShares(
+  v: [string, string] | null,
+  c: [string | undefined, string | undefined] | undefined,
+  vaultSymbol: string
+) {
+  if (!v) return undefined;
+  if (!c) return undefined;
+  if (c[0] !== undefined && c[1] === undefined) {
+    return [TypedBigNumber.from(v[0], BigNumberType.DebtShare, `${vaultSymbol}:${c[0]}`).toJSON()];
+  }
+  if (c[0] !== undefined && c[1] !== undefined) {
+    return [
+      TypedBigNumber.from(v[0], BigNumberType.DebtShare, `${vaultSymbol}:${c[0]}`).toJSON(),
+      TypedBigNumber.from(v[1], BigNumberType.DebtShare, `${vaultSymbol}:${c[1]}`).toJSON(),
+    ];
+  }
   return undefined;
 }
 
@@ -395,6 +448,8 @@ export async function getSystemConfig(graphClient: GraphClient): Promise<Currenc
             : undefined,
           strategyVaults: c.strategyVaults.map((v) => {
             const symbol = underlyingSymbol(c);
+            const secondarySymbols = mapSecondarySymbols(v.secondaryBorrowCurrencies);
+
             return {
               ...v,
               primaryBorrowCurrency: Number(v.primaryBorrowCurrency.id),
@@ -402,19 +457,27 @@ export async function getSystemConfig(graphClient: GraphClient): Promise<Currenc
                 v.minAccountBorrowSize,
                 BigNumberType.InternalUnderlying,
                 symbol
-              ),
+              ).toJSON(),
               maxPrimaryBorrowCapacity: TypedBigNumber.from(
                 v.vaultCapacity.maxPrimaryBorrowCapacity,
                 BigNumberType.InternalUnderlying,
                 symbol
-              ),
+              ).toJSON(),
               totalUsedPrimaryBorrowCapacity: TypedBigNumber.from(
                 v.vaultCapacity.totalUsedPrimaryBorrowCapacity,
                 BigNumberType.InternalUnderlying,
                 symbol
-              ),
+              ).toJSON(),
               secondaryBorrowCurrencies: mapSecondaryBorrowCurrencies(v.secondaryBorrowCurrencies),
-              maturities: v.maturities.map((m) => {
+              maxSecondaryBorrowCapacity: mapSecondaryTypedBigNumber(
+                v.vaultCapacity.maxSecondaryBorrowCapacity,
+                secondarySymbols
+              ),
+              totalUsedSecondaryBorrowCapacity: mapSecondaryTypedBigNumber(
+                v.vaultCapacity.totalUsedSecondaryBorrowCapacity,
+                secondarySymbols
+              ),
+              vaultStates: v.maturities.map((m) => {
                 const assetSymbol = c.symbol;
                 const vaultSymbol = `${v.vaultAddress}:${m.maturity}`;
                 return {
@@ -423,25 +486,62 @@ export async function getSystemConfig(graphClient: GraphClient): Promise<Currenc
                     m.totalPrimaryfCashBorrowed,
                     BigNumberType.InternalUnderlying,
                     symbol
+                  ).toJSON(),
+                  totalSecondaryfCashBorrowed: mapSecondaryTypedBigNumber(
+                    m.totalSecondaryfCashBorrowed,
+                    secondarySymbols
                   ),
-                  totalAssetCash: TypedBigNumber.from(m.totalAssetCash, BigNumberType.InternalAsset, assetSymbol),
-                  totalVaultShares: TypedBigNumber.from(m.totalVaultShares, BigNumberType.VaultShare, vaultSymbol),
+                  totalSecondaryDebtShares: mapSecondaryDebtShares(
+                    m.totalSecondaryDebtShares,
+                    secondarySymbols,
+                    vaultSymbol
+                  ),
+                  totalAssetCash: TypedBigNumber.from(
+                    m.totalAssetCash,
+                    BigNumberType.InternalAsset,
+                    assetSymbol
+                  ).toJSON(),
+                  totalVaultShares: TypedBigNumber.from(
+                    m.totalVaultShares,
+                    BigNumberType.VaultShare,
+                    vaultSymbol
+                  ).toJSON(),
                   totalStrategyTokens: TypedBigNumber.from(
                     m.totalStrategyTokens,
                     BigNumberType.StrategyToken,
                     vaultSymbol
-                  ),
+                  ).toJSON(),
                   settlementStrategyTokenValue: m.settlementStrategyTokenValue
-                    ? TypedBigNumber.from(m.settlementStrategyTokenValue, BigNumberType.InternalUnderlying, symbol)
+                    ? TypedBigNumber.from(
+                        m.settlementStrategyTokenValue,
+                        BigNumberType.InternalUnderlying,
+                        symbol
+                      ).toJSON()
                     : undefined,
+                  settlementSecondaryBorrowfCashSnapshot: mapSecondaryTypedBigNumber(
+                    m.settlementSecondaryBorrowfCashSnapshot,
+                    // Secondary borrow fCash snapshots are in underlying
+                    secondarySymbols?.map((s) => (s ? symbol : undefined)) as
+                      | [string | undefined, string | undefined]
+                      | undefined
+                  ),
+                  settlementRate: m.settlementRate?.rate ? BigNumber.from(m.settlementRate.rate) : undefined,
                   shortfall: m.shortfall
-                    ? TypedBigNumber.from(m.shortfall, BigNumberType.InternalAsset, assetSymbol)
+                    ? TypedBigNumber.from(m.shortfall, BigNumberType.InternalAsset, assetSymbol).toJSON()
                     : undefined,
                   remainingSettledAssetCash: m.remainingSettledAssetCash
-                    ? TypedBigNumber.from(m.remainingSettledAssetCash, BigNumberType.InternalAsset, assetSymbol)
+                    ? TypedBigNumber.from(
+                        m.remainingSettledAssetCash,
+                        BigNumberType.InternalAsset,
+                        assetSymbol
+                      ).toJSON()
                     : undefined,
                   remainingSettledStrategyTokens: m.remainingSettledStrategyTokens
-                    ? TypedBigNumber.from(m.remainingSettledStrategyTokens, BigNumberType.StrategyToken, vaultSymbol)
+                    ? TypedBigNumber.from(
+                        m.remainingSettledStrategyTokens,
+                        BigNumberType.StrategyToken,
+                        vaultSymbol
+                      ).toJSON()
                     : undefined,
                 };
               }),
